c6158f25989f8e4a7f26d6022f6177103fe21554
AnthonyCabrera <anthony.cabrera2411@gmail.com>
2026-03-17 07:51:43 -0600

    fix chrome en railway

--- scraper.py
+++ scraper.py
@@ -13,14 +13,16 @@ ya_enviados = cargar_enviados()
 
 def crear_driver():
     options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
     options.add_argument("--lang=es-MX")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option("useAutomationExtension", False)
-    options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+    options.binary_location = "/usr/bin/chromium"  # <- ruta en Railway
+    
     driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
+        service=Service("/usr/bin/chromedriver"),  # <- ruta en Railway
         options=options
     )
     return driver
