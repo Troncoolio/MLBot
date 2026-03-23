3189e622fbd3cbc982dc8082c02f948a166f577a
AnthonyCabrera <anthony.cabrera2411@gmail.com>
2026-03-22 18:19:39 -0600

    fix matt_word afiliado

--- afiliados.py
+++ afiliados.py
@@ -1,11 +1,14 @@
 from urllib.parse import urlencode
-from config import MATT_TOOL
+import os
+
+MATT_TOOL = os.getenv("MATT_TOOL", "78642436")
+MATT_WORD = "caan3223614"
 
 def agregar_afiliado(link):
     link_limpio = link.split("#")[0].split("?")[0]
     params = urlencode({
         "matt_tool": MATT_TOOL,
-        "matt_word": "",
+        "matt_word": MATT_WORD,
         "matt_source": "copy_link",
         "matt_campaign": "native-ads"
     })
