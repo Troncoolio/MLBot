e4d20a66cba02e4fb8e0651d841d797f5008123c
AnthonyCabrera <anthony.cabrera2411@gmail.com>
2026-03-22 18:30:14 -0600

    link de afiliado correccion

--- afiliados.py
+++ afiliados.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlencode, quote
+from urllib.parse import urlencode
 import os
 
 MATT_TOOL = os.getenv("MATT_TOOL", "78642436")
@@ -7,9 +7,9 @@ MATT_WORD = "caan3223614"
 def agregar_afiliado(link):
     link_limpio = link.split("#")[0].split("?")[0]
     params = urlencode({
-        "matt_word": MATT_WORD,
         "matt_tool": MATT_TOOL,
-        "forceInApp": "true",
-        "url": link_limpio  # <- el producto va aquí
+        "matt_word": MATT_WORD,
+        "matt_source": "copy_link",
+        "matt_campaign": "native-ads"
     })
-    return f"https://www.mercadolibre.com.mx/social/{MATT_WORD}?{params}"
+    return f"{link_limpio}?{params}"
