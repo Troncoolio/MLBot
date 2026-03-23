fc52ccd5d2596fdecd2b383ffd84adc326c5c8e6
AnthonyCabrera <anthony.cabrera2411@gmail.com>
2026-03-22 18:25:57 -0600

    link de afiliado

--- afiliados.py
+++ afiliados.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlencode
+from urllib.parse import urlencode, quote
 import os
 
 MATT_TOOL = os.getenv("MATT_TOOL", "78642436")
@@ -7,9 +7,9 @@ MATT_WORD = "caan3223614"
 def agregar_afiliado(link):
     link_limpio = link.split("#")[0].split("?")[0]
     params = urlencode({
-        "matt_tool": MATT_TOOL,
         "matt_word": MATT_WORD,
-        "matt_source": "copy_link",
-        "matt_campaign": "native-ads"
+        "matt_tool": MATT_TOOL,
+        "forceInApp": "true",
+        "url": link_limpio  # <- el producto va aquí
     })
-    return f"{link_limpio}?{params}"
+    return f"https://www.mercadolibre.com.mx/social/{MATT_WORD}?{params}"
