eeefdb50dd01f48306e499866862530046c93bbe
AnthonyCabrera <anthony.cabrera2411@gmail.com>
2026-03-23 12:33:24 -0600

    Arregla problemas de identacion

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ def ronda():
     total = buscar_ofertas()
     logger.info(f"Ronda completa ofertas {total}")
 
-    schedule.every().day.at("8:30").do(ronda)
-    schedule.every().day.at("10:40").do(ronda)
-    schedule.every().day.at("13:30").do(ronda)
-    schedule.every().day.at("19:30").do(ronda)
+schedule.every().day.at("8:30").do(ronda)
+schedule.every().day.at("10:40").do(ronda)
+schedule.every().day.at("13:30").do(ronda)
+schedule.every().day.at("19:30").do(ronda)
 
-    logger.info("Iniciado, espera horario")
+logger.info("Iniciado, espera horario")
 
+ronda()
 
 while True:
     schedule.run_pending()
