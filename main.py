b5a999346b4fa40546ada6b58c8b9cf9fc20ff3b
AnthonyCabrera <anthony.cabrera2411@gmail.com>
2026-03-23 12:46:05 -0600

    Cambiando horario y agregando descmax

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def ronda():
 
 schedule.every().day.at("8:30").do(ronda)
 schedule.every().day.at("10:40").do(ronda)
+schedule.every().day.at("12:50").do(ronda)
 schedule.every().day.at("13:30").do(ronda)
 schedule.every().day.at("19:30").do(ronda)
 
